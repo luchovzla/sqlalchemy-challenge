1fc0c9880452ea5f6ff5a98541379c0907cbc46b
Luis Gomez <luis.g.1990@gmail.com>
2021-03-19 12:22:04 -0500

    Completed Flask API and Bonus Analysis 1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Dependencies
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -84,6 +85,87 @@ def station_query():
 @app.route("/api/v1.0/tobs")
 def temperature_list():
 
+    # Open a session
+    session = Session(engine)
+
+    # Query temp observations
+    tobs_query = session.query(Measurement.station, Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == "USC00519281", Measurement.date >= "2016-08-23").all()
+
+    # Close session
+    session.close()
+
+    # Parse TOBS into a list
+    tobs_list = []
+    for record in tobs_query:
+        tobs = record[2]
+        tobs_list.append(tobs)
+
+    # Return list
+    return jsonify(tobs_list)
+
+# Dynamic API
+@app.route("/api/v1.0/<start>") 
+
+def start_temp_summary(start):
+
+     # Open a session
+    session = Session(engine)
+
+    # Query temp observations
+    tobs_query = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= start).all()
+
+    # Close session
+    session.close()
+
+    # Retrieve a list of temperatures
+    tobs_list = []
+    for record in tobs_query:
+        tobs = record[1]
+        if tobs == 'null':
+            pass
+        else:
+            tobs_list.append(tobs)
+
+    tobs_summary = {
+        'TMIN': min(tobs_list),
+        'TAVG': np.average(tobs_list),
+        'TMAX': max(tobs_list)
+        }
+
+    return jsonify(tobs_summary)
+    
+@app.route("/api/v1.0/<start>/<end>") 
+
+def start_end_temp_summary(start, end):
+
+     # Open a session
+    session = Session(engine)
+
+    # Query temp observations
+    tobs_query = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= start, Measurement.date <= end).all()
+
+    # Close session
+    session.close()
+
+    # Retrieve a list of temperatures
+    tobs_list = []
+    for record in tobs_query:
+        tobs = record[1]
+        if tobs == 'null':
+            pass
+        else:
+            tobs_list.append(tobs)
+
+    tobs_summary = {
+        'TMIN': min(tobs_list),
+        'TAVG': np.average(tobs_list),
+        'TMAX': max(tobs_list)
+        }
+        
+    return jsonify(tobs_summary)
 
 # Debug mode
 if __name__ == "__main__":
